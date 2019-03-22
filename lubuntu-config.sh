5154cac5322bf6304944a7c7d1e7dad54887fd80
Drew Ku <andrewku123@gmail.com>
2019-03-21 20:47:54 -0400

    additions

--- lubuntu-config.sh
+++ lubuntu-config.sh
@@ -1,5 +1,31 @@
 #git clone https://github.com/Drookoo/cyedpi
 
 # to use ifconfig 
-sudo apt get install net-tools 
+sudo apt install net-tools -y
+sudo apt install tightvncserver -y
+sudo apt install openssh-server -y
 
+#change passwd 
+passwd lubuntu 
+#current passwd is blank
+#lubuntu1
+#lubuntu1
+
+#configure 
+vncserver 
+#password "lubuntu", no view only password 
+
+#check tightvnc is running on 5901 
+netstat -tulpa 
+
+#check ssh is running 
+sudo systemctl status ssh 
+
+#install RealVNC viewer, download its .deb 
+#cd Downloads 
+#dpkg -i .....
+
+#Run
+vncconnect [address:port] 
+vncviewer 
+#provide address:port 
